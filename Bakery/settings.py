1875786a95cd4291c935bbdc7b57a2de088a5cf0
Moin <moinkhan8439@gmail.com>
2021-03-01 17:24:32 +0530

    adding allowed host

--- Bakery/settings.py
+++ Bakery/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://bakery-management-api.herokuapp.com/',]
 
 
 # Application definition
