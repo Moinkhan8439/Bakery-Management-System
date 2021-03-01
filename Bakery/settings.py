5fe66cdaad6673ac4dd6d765729cf0b5e5b8720d
Moin <moinkhan8439@gmail.com>
2021-03-01 18:07:47 +0530

    Secret Key shown

--- Bakery/settings.py
+++ Bakery/settings.py
@@ -21,13 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '&12_yx9r8lnb)6)w8j7-5gub+kbm*$^q84bqzdx-tw(x&j+am2'
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = '&12_yx9r8lnb)6)w8j7-5gub+kbm*$^q84bqzdx-tw(x&j+am2'
+#SECRET_KEY = os.environ.get['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ('DEBUG')
+#DEBUG = os.environ.get['DEBUG']
+DEBUG=True
 
-ALLOWED_HOSTS = ['https://bakery-management-api.herokuapp.com/',]
+ALLOWED_HOSTS = ['https://bakery-management-api.herokuapp.com/','127.0.0.1:8000/']
 
 
 # Application definition
