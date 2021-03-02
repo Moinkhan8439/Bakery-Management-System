df19ea7478cff43d0fd7cc14bea0535babb54685
Moin <moinkhan8439@gmail.com>
2021-03-02 11:40:49 +0530

    deleting secret key

--- Bakery/settings.py
+++ Bakery/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '&12_yx9r8lnb)6)w8j7-5gub+kbm*$^q84bqzdx-tw(x&j+am2'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
